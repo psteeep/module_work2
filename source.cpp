0b2f13dff53f88e4489764e26f6cba2ccf533da5
psteeep <psteeep@users.noreply.github.com>
2021-05-14 14:55:24 +0300

    update version without file

--- source.cpp
+++ source.cpp
@@ -12,20 +12,31 @@ int main() {
     //    out << "asdkjqjdlkqejdeifjewlfkwjelk" << std::endl;
     //}
 
-    char S[50]; 
+    char S[] = "dwdwefweqf23ewdkdlwjwlfjwq0wsad"; 
     char l1;
     char l2;
     
     int num_l1 = 0;
-    int num_l2 = 2;
+    int num_l2 = 0;
     
-    // ввід рядка S
-  
-    // ввід символу a
-    // ...
- 
-    for (int i = 0; i < strlen(S); i++)
-        if (S[i] == l1)
-           num_l1++; 
+    std::cout << "enter l1 and l2: ";
+    std::cin >> l1;
+    std::cin >> l2;
+
+
+    for (int i = 0; i < strlen(S); i++) {
+        if (S[i] == l1) {
+            num_l1++;
+        }
+        if (S[i] == l2) {
+            num_l2++;
+        }
+        else
+            std::cout << "no like that" << std::endl;
+    }
+    std::cout << "symbol " << l1 << " = " << num_l1 << " times!!" << std::endl;
+    std::cout << "symbol " << l2 << " = " << num_l2 << " times!!" << std::endl;
+
+
 	return 0;
 }
