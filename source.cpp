e07c0562eb5d01069274c10bd1754b9a654c5a2b
psteeep <psteeep@users.noreply.github.com>
2021-05-14 14:32:57 +0300

    add info in txt file

--- source.cpp
+++ source.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
+
+
+    std::ofstream out;          
+    out.open("testing.txt"); 
+    if (out.is_open())
+    {
+        out << "asdkjqjdlkqejdeifjewlfkwjelk" << std::endl;
+    }
 
 
 	return 0;
