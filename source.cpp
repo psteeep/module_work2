b102b6099d1fc24b407dda2e4dfd95c5d668e08d
psteeep <psteeep@users.noreply.github.com>
2021-05-14 14:46:31 +0300

    testing without file

--- source.cpp
+++ source.cpp
@@ -5,13 +5,27 @@
 int main() {
 
 
-    std::ofstream out;          
-    out.open("testing.txt"); 
-    if (out.is_open())
-    {
-        out << "asdkjqjdlkqejdeifjewlfkwjelk" << std::endl;
-    }
+    //std::ofstream out;          
+    //out.open("testing.txt"); 
+    //if (out.is_open())
+    //{
+    //    out << "asdkjqjdlkqejdeifjewlfkwjelk" << std::endl;
+    //}
 
-
+    char S[50]; 
+    char l1;
+    char l2;
+    
+    int num_l1 = 0;
+    int num_l2 = 2;
+    
+    // ââ³ä ğÿäêà S
+  
+    // ââ³ä ñèìâîëó a
+    // ...
+ 
+    for (int i = 0; i < strlen(S); i++)
+        if (S[i] == l1)
+           num_l1++; 
 	return 0;
 }
